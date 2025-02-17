64f15e213fdd582689548059e249cc7183d2e4ee
Kharlouuu <karlharlou132@gmail.com>
2025-02-17 14:43:23 +0800

    0217_6_arrays_lec

--- homework-solution.php
+++ homework-solution.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <title>Demo</title>
 </head>
+
 <body>
-    <h1>
-        <?= 'Hello World' ?>
-    </h1>
+    <?php
+        $usernames = [
+            "JohnDoe",
+            "JaneDoe",
+            "FrankDoe"
+        ];
+    ?>
+
+    <h1>Top Performing Users</h1>
+
+    <ol>
+        <?php foreach ($usernames as $username) : ?>
+            <li><?= $username ?></li>
+        <?php endforeach; ?>
+    </ol>
+
 </body>
 </html>
