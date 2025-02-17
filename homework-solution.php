2f3325b07374636bdf353450b496d2be4c476c23
Kharlouuu <karlharlou132@gmail.com>
2025-02-17 14:44:59 +0800

    0217_7_associative-arrays_lec

--- homework-solution.php
+++ homework-solution.php
@@ -7,20 +7,30 @@
 
 <body>
     <?php
-        $usernames = [
-            "JohnDoe",
-            "JaneDoe",
-            "FrankDoe"
+        $books = [
+            [
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => 1968,
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaseUrl' => 'http://example.com'
+            ]
         ];
     ?>
 
-    <h1>Top Performing Users</h1>
-
-    <ol>
-        <?php foreach ($usernames as $username) : ?>
-            <li><?= $username ?></li>
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <li>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>)
+                </a>
+            </li>
         <?php endforeach; ?>
-    </ol>
-
+    </ul>
 </body>
 </html>
