04cff1e520f591cd8422bb13e89d2647e9a088b3
Kharlouuu <karlharlou132@gmail.com>
2025-02-17 14:41:51 +0800

    0217_5_conditionals-and-booleans_lec

--- homework-solution.php
+++ homework-solution.php
@@ -5,15 +5,8 @@
     <title>Demo</title>
 </head>
 <body>
-
     <h1>
-        <?php
-            $greeting = "Hello";
-            $noun = "World";
-
-            echo "$greeting $noun" . "!"
-        ?> 
+        <?= 'Hello World' ?>
     </h1>
-
 </body>
 </html>
